4ef98f0ef0db3d1278124fe79e8c543392f4bc59
정현우 <gusehd502@naver.com>
2023-05-19 15:45:15 +0900

    inclass

--- final_pjt_back/movies/models.py
+++ final_pjt_back/movies/models.py
@@ -4,31 +4,29 @@ class TotalMovies(models.Model):
     title = models.CharField(max_length=100)
     movie_id = models.IntegerField(primary_key=True)
     release_date = models.DateField(blank=True)
-    overview = models.TextField()
-    poster_path = models.CharField(max_length=200)
-    vote_average = models.FloatField()
+    overview = models.TextField(blank=True)
+    poster_path = models.TextField(blank=True)
+    vote_average = models.FloatField(default=0)
     popularity = models.FloatField()
-    genres = models.CharField(max_length=100)
-    youtube_key = models.CharField(max_length=200)
-    runtime = models.TimeField()
+    genres = models.CharField(max_length=100, blank=True)
+    youtube_key = models.CharField(max_length=500)
+    runtime = models.CharField(max_length=50)
     
 
 class PopularMovies(models.Model):
     title = models.CharField(max_length=100)
-    # movie_id = models.IntegerField(primary_key=True)
     release_date = models.DateField(blank=True)
-    overview = models.TextField()
-    poster_path = models.CharField(max_length=200)
-    vote_average = models.FloatField()
+    overview = models.TextField(blank=True)
+    poster_path = models.TextField(blank=True)
+    vote_average = models.FloatField(default=0)
     popularity = models.FloatField()
 
 
 class TopRatedMovies(models.Model):
     title = models.CharField(max_length=100)
-    # movie_id = models.IntegerField(primary_key=True)
     release_date = models.DateField(blank=True)
-    overview = models.TextField()
-    poster_path = models.CharField(max_length=200)
-    vote_average = models.FloatField()
+    overview = models.TextField(blank=True)
+    poster_path = models.TextField(blank=True)
+    vote_average = models.FloatField(default=0)
     popularity = models.FloatField()
 
